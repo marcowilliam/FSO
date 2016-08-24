a698c217847dd11f5eb5e232ddade9b6f25ccc98
Marco Silva <marcowilliamdf@gmail.com>
2016-08-24 10:03:32 -0300

    adding some texts to the solution

--- marco_silva_120037416_matheus_godinho_xxxxxxx_trab01/q03/main.c
+++ marco_silva_120037416_matheus_godinho_xxxxxxx_trab01/q03/main.c
@@ -7,17 +7,29 @@ int main(){
     double number1 = 7.3, number2;
     char s1[100], s2[100];
     double* dPtr;
+
     dPtr = &number1;
-    printf("Valor da variável apontada por 'dPtr': %f\n", *dPtr);
+
+    printf("\nValor da variável apontada por 'dPtr': %f\n", *dPtr);
+
     number2 = *dPtr;
+
     printf("Valor da varável 'number2': %f\n", number2);
     printf("Endereco de 'number1': %p\n", &number1);
     printf("Endereco de 'dPtr': %p\n", dPtr);
+    printf("\nO valor impresso decorrente do enunciado que contempla o item anterior");
+    printf(" é igual ao valor do endereço gravado em dPrt?\nR.: Sim\n");
+
+    printf("\nEntre com a string:\n");
     scanf("%s", s1);
+
     strcpy(s2,s1);
-    printf("%d\n", strcmp(s1,s2)); 
+
+    printf("Resultado da comparacao entre as duas strings: %d\n", strcmp(s1,s2)); 
+    
     strcat(s1,s2);
-    printf("%s\n", s1);
-    printf("%lu\n", strlen(s1));
+    printf("As duas strings concatenadas: %s\n", s1);
+
+    printf("Comprimento da string armazenada em s1: %lu\n", strlen(s1));
     return 0;
 }
